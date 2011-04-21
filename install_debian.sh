ffe9ca76b94d1d6413359e7a7588a135ccf882b1
Ettore Del Negro <write@ettoredelnegro.me>
2011-04-20 21:32:35 -0400

    * Check root privileges for install

--- install_debian.sh
+++ install_debian.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 REQUIRED_PACKAGES="php5-fpm apache2-mpm-worker selinux-basics"
 
+# Script must be run as root
+if [[ $( whoami ) != "root" ]]
+then
+	echo "*** This script must be run as root" >&2 && exit 1
+fi
+
 # Check OS
 if [[ ! -f /etc/debian_version || $( cat /etc/debian_version | cut -d. -f1 ) != "6" ]]
 then
-	echo -e "*** This scripts need to be run on Debian Squeeze 6" >&2 && exit 1
+	echo "*** This script needs to be run on Debian Squeeze 6" >&2 && exit 1
 fi
 
 MISSING_PKGS=0
