7ad17d0724280d7ebe3345d8649fa0cc4d797526
Ettore Del Negro <write@ettoredelnegro.me>
2011-04-22 16:30:11 -0400

    * Code cleanups

--- php5-sephp/sephp.c
+++ php5-sephp/sephp.c
@@ -78,53 +78,53 @@ PHP_MINFO_FUNCTION(sephp)
 
 void sephp_php_import_environment_variables(zval *array_ptr TSRMLS_DC)
 {
-    char *fcgi_params[SEPHP_PARAMS_COUNT] = { SEPHP_PARAMS };
-    char *fcgi_values[SEPHP_PARAMS_COUNT];
-    zval **data;
-    HashTable *arr_hash;
-    HashPosition pointer;
-    int i;
+	char *fcgi_params[SEPHP_PARAMS_COUNT] = { SEPHP_PARAMS };
+	char *fcgi_values[SEPHP_PARAMS_COUNT];
+	zval **data;
+	HashTable *arr_hash;
+	HashPosition pointer;
+	int i;
     
-    /* call php's original import as a catch-all */
+	/* call php's original import as a catch-all */
 	old_php_import_environment_variables(array_ptr TSRMLS_CC);
 	
-    arr_hash = Z_ARRVAL_P(array_ptr);
-    for (zend_hash_internal_pointer_reset_ex(arr_hash, &pointer); 
-    	zend_hash_get_current_data_ex(arr_hash, (void**) &data, &pointer) == SUCCESS; 
-    	zend_hash_move_forward_ex(arr_hash, &pointer))
-    {
-    	char *key;
-    	int key_len;
+	arr_hash = Z_ARRVAL_P(array_ptr);
+	for (zend_hash_internal_pointer_reset_ex(arr_hash, &pointer); 
+		zend_hash_get_current_data_ex(arr_hash, (void**) &data, &pointer) == SUCCESS; 
+		zend_hash_move_forward_ex(arr_hash, &pointer))
+	{
+		char *key;
+		int key_len;
 		long index;
 		
-    	if (zend_hash_get_current_key_ex(arr_hash, &key, &key_len, &index, 0, &pointer) == HASH_KEY_IS_STRING)
-    	{
-     		for (i=0; i < SEPHP_PARAMS_COUNT; i++)
-     		{
-     			if (!strncmp( key, fcgi_params[i], strlen(fcgi_params[i])))
-     			{
-     				// TODO handle of other types (int, null, etc)
+		if (zend_hash_get_current_key_ex(arr_hash, &key, &key_len, &index, 0, &pointer) == HASH_KEY_IS_STRING)
+		{
+			for (i=0; i < SEPHP_PARAMS_COUNT; i++)
+			{
+				if (!strncmp( key, fcgi_params[i], strlen(fcgi_params[i])))
+				{
+					// TODO handle of other types (int, null, etc)
 					if (Z_TYPE_PP(data) == IS_STRING)
-						fcgi_values[i] = Z_STRVAL_PP(data);
-     			}
-     		}
-    	}
-    }
-    
-    // TODO selinux stuff (set context, etc)
-    for (i=0; i < SEPHP_PARAMS_COUNT; i++)
-    {
- 		if (fcgi_values[i])
- 		{
-//  			char buf[500];
-//  			memset( buf, 0, sizeof(buf) );
-//  			sprintf( buf, "SELINUX %s => %s <br>", fcgi_params[i], fcgi_values[i] );
-//  			PHPWRITE( buf, strlen(buf) );
+					fcgi_values[i] = Z_STRVAL_PP(data);
+				}
+			}
+		}
+	}
+	 
+	// TODO selinux stuff (set context, etc)
+	for (i=0; i < SEPHP_PARAMS_COUNT; i++)
+	{
+		if (fcgi_values[i])
+		{
+// 			char buf[500];
+// 			memset( buf, 0, sizeof(buf) );
+// 			sprintf( buf, "SELINUX %s => %s <br>", fcgi_params[i], fcgi_values[i] );
+// 			PHPWRITE( buf, strlen(buf) );
 		}
 	}
 
-	// Don't expose SePHP parameters to scripts through $_SERVER    
-    for (i=0; i < SEPHP_PARAMS_COUNT; i++)
- 		if (fcgi_values[i])
- 			zend_hash_del(arr_hash, fcgi_params[i], strlen(fcgi_params[i]) + 1);
+	// Don't expose SePHP parameters to scripts through $_SERVER	 
+	for (i=0; i < SEPHP_PARAMS_COUNT; i++)
+		if (fcgi_values[i])
+			zend_hash_del(arr_hash, fcgi_params[i], strlen(fcgi_params[i]) + 1);
 }
