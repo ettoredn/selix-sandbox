e9bc186f4c559e3f4e04f5f4d715ed5b518a0a8f
Ettore Del Negro <write@ettoredelnegro.me>
2011-06-08 20:47:53 -0400

    + require selinux-policy-dev package

--- install_debian.sh
+++ install_debian.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Configuration variables
 PHP_MODULES_PATH="/usr/lib/php5/20090626"
-REQUIRED_PACKAGES="php5-fpm php5-dev apache2-mpm-worker libapache2-mod-fastcgi nginx selinux-basics libselinux1-dev gawk"
+REQUIRED_PACKAGES="php5-fpm php5-dev apache2-mpm-worker libapache2-mod-fastcgi nginx selinux-basics libselinux1-dev selinux-policy-dev gawk"
 REQUIRED_APACHE_MODS="actions fastcgi"
 SKIP_APACHE=0
 SKIP_NGINX=0
