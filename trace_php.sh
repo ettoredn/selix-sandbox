e17e01bf40c377a85abecd42796a7388714c1150
Ettore Del Negro <write@ettoredelnegro.me>
2012-04-12 15:11:20 +0200

    Added bash script to automatically create and show a trace session for php-cli

---
+++ trace_php.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+TRACES_DIR="/dev/shm/traces"
+BABELTRACE_ARGS="--clock-seconds"
+ENV_ARGS="LD_PRELOAD=liblttng-ust-fork.so\
+ SELINUX_DOMAIN=sephp_php_t\
+ SELINUX_RANGE=s0\
+ SELINUX_COMPILE_DOMAIN=sephp_php_t\
+ SELINUX_COMPILE_RANGE=s0"
+
+function usage {
+	echo "Usage: $0 [filename]"
+	exit 1
+}
+
+function quit {
+	cd "$old_cwd"
+	if (( $1 > 0 )) ; then echo -e "\n*** Aborted due to previous errors" ; fi
+	exit $1
+}
+
+# Initialize variables
+old_cwd=$( pwd )
+abspath=$(cd ${0%/*} && echo $PWD/${0##*/})
+cwd=$( dirname "$abspath" )
+ecwd=$( echo $cwd | sed 's/\//\\\//g' )
+scriptname="$1"
+lttng_session=$( date +%s )
+tracepath="$TRACES_DIR/cli"
+
+# Change to script directory
+cd "$cwd"
+
+# SELinux should be disabled
+if [[ $( cat /selinux/enforce ) != 0 ]]
+then
+	echo "*** SELinux must be disabled" >&2 && quit 1
+fi
+
+# Check LTTng v2
+if [[ $( lttng -h | head -1 | cut -d" " -f4 | cut -d. -f1 ) != 2 ]]
+then
+	echo "*** LTTng-2.0 is needed to trace PHP" >&2 && quit 1
+fi
+
+# Check php-cli
+which php &>/dev/null || ( echo "*** Can't locate php-cli" >&2 && quit 1 )
+
+# Create LTTng session
+rm -rf "$tracepath" && mkdir -p "$tracepath" || quit 1
+lttng create --output "$tracepath" "$lttng_session" >/dev/null || quit 1
+lttng enable-event -a -u >/dev/null || quit 1
+lttng start "$lttng_session" >/dev/null || quit 1
+# Run PHP
+cmd="env $ENV_ARGS php $scriptname"
+$cmd >/dev/null || quit 1
+# Destroy LTTng session
+lttng stop "$lttng_session" >/dev/null || quit 1
+lttng destroy "$lttng_session" >/dev/null || quit 1
+
+# Display trace
+babeltrace $BABELTRACE_ARGS "$TRACES_DIR/cli"
+
+quit 0
